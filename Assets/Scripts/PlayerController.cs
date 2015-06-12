8bc8158367ff9d5ef1a8fcc083e082743ffadd93
Kristofer Liljeblad <kristol@microsoft.com>
2015-06-11 18:57:34 -0700

    changed movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -27,25 +27,34 @@ public class PlayerController : MonoBehaviour {
     public float tilt;
     public Boundary boundary;
     new Rigidbody rigidbody;
-
+    public float turnSpeed;
+    public float thrustSpeed;
 
     public void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
+        //Debug.Log("FixedUpdate()");
+        //Debug.Log(moveHorizontal);
 
+        transform.Rotate(0, moveHorizontal * turnSpeed, 0);
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        rigidbody.velocity = movement * speed;
+        float thrust = moveVertical * thrustSpeed;
+        rigidbody.AddForce(transform.forward * thrust);
 
-        rigidbody.position = new Vector3
-        (
-            Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax),
-            0.0f,
-            Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax)
-        );
+        //rigidbody.rotation = Quaternion.Euler(0.0f, rigidbody.velocity.x * -tilt * Mathf.Abs(rigidbody.position.x / 2f), 0.0f);
 
-        rigidbody.rotation = Quaternion.Euler(0.0f, 0.0f, rigidbody.velocity.x * -tilt * Mathf.Abs(rigidbody.position.x / 2f));
+        //Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        //rigidbody.velocity = movement * speed;
+
+        //rigidbody.position = new Vector3
+        //(
+        //    Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax),
+        //    0.0f,
+        //    Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax)
+        //);
+
+        //rigidbody.rotation = Quaternion.Euler(0.0f, 0.0f, rigidbody.velocity.x * -tilt * Mathf.Abs(rigidbody.position.x / 2f));
 
 
     }
