d86d4b737f3bff51dba88924f359820f132b7787
Kristofer Liljeblad <kristol@microsoft.com>
2015-06-11 19:24:17 -0700

    added ability to shot fricking laser shots

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+    // Use this for initialization
+    public float speed;
+    new Rigidbody rigidbody;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = transform.forward * speed;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,18 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+    private float nextFire;
+
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+    new Rigidbody rigidbody;
+    public float turnSpeed;
+    public float thrustSpeed;
+
     [System.Serializable]
     public class Boundary
     {
@@ -20,42 +32,24 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        }
+    }
 
-    public float speed;
-    public float tilt;
-    public Boundary boundary;
-    new Rigidbody rigidbody;
-    public float turnSpeed;
-    public float thrustSpeed;
+
 
     public void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-        //Debug.Log("FixedUpdate()");
-        //Debug.Log(moveHorizontal);
 
         transform.Rotate(0, moveHorizontal * turnSpeed, 0);
 
         float thrust = moveVertical * thrustSpeed;
         rigidbody.AddForce(transform.forward * thrust);
-
-        //rigidbody.rotation = Quaternion.Euler(0.0f, rigidbody.velocity.x * -tilt * Mathf.Abs(rigidbody.position.x / 2f), 0.0f);
-
-        //Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        //rigidbody.velocity = movement * speed;
-
-        //rigidbody.position = new Vector3
-        //(
-        //    Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax),
-        //    0.0f,
-        //    Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax)
-        //);
-
-        //rigidbody.rotation = Quaternion.Euler(0.0f, 0.0f, rigidbody.velocity.x * -tilt * Mathf.Abs(rigidbody.position.x / 2f));
-
 
     }
 
